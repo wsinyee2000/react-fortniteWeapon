8879adec0c7649e644cac1c7b68dc70f5fde1ac2
wsinyee2000 <62838083+wsinyee2000@users.noreply.github.com>
2020-10-07 22:49:57 +1100

    Updates

---
+++ precache-manifest.a0e04d92d841b01110089127814684e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09482296a4f54af3a1c019705edd9c67",
+    "url": "/react-fortniteWeapon/index.html"
+  },
+  {
+    "revision": "e84a946e81de7777af08",
+    "url": "/react-fortniteWeapon/static/css/main.5f45ab8f.chunk.css"
+  },
+  {
+    "revision": "3d9108964b689a04af44",
+    "url": "/react-fortniteWeapon/static/js/2.02cc5931.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-fortniteWeapon/static/js/2.02cc5931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84a946e81de7777af08",
+    "url": "/react-fortniteWeapon/static/js/main.09f986d7.chunk.js"
+  },
+  {
+    "revision": "53d751d11cf49eee0f67",
+    "url": "/react-fortniteWeapon/static/js/runtime-main.7f947090.js"
+  },
+  {
+    "revision": "5e6bf5cd0feb76454773438e8e935722",
+    "url": "/react-fortniteWeapon/static/media/home_img.5e6bf5cd.jpg"
+  },
+  {
+    "revision": "9d15133ca8a311f4b5b178ac52f15dd0",
+    "url": "/react-fortniteWeapon/static/media/toppng.com-fortnite-logo-vector-download-logo-fortnite-1600x1599.9d15133c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-fortniteWeapon/precache-manifest.1d28bab175955bccd000f6534fdce536.js"
+  "/react-fortniteWeapon/precache-manifest.a0e04d92d841b01110089127814684e9.js"
 );
 
 self.addEventListener('message', (event) => {
